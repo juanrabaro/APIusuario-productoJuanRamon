64154a59c71127842df2917ab6eb96dbf8da0510
juanrabaro <jrbaro@iesrafaelalberti.es>
2023-11-09 21:05:50 +0100

    puerto 3000 no funciona

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:63342")
+                .allowedOrigins("http://localhost:3003")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
